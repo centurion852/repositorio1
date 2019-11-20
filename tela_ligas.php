1e090729b14730f5d1944060f9895868739d049e
Boa-Noite <Boa-Noite@users.noreply.github.com>
2019-11-19 21:22:17 -0400

    ACABOU MINHA PARTE MAN

--- tela_ligas.php
+++ tela_ligas.php
@@ -10,3 +10,73 @@ lms
 vcs
 
 -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Ligas</title>
+        <link rel="icon" type="image/png"  href="imagens/logo.png">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body class="bg-dark">
+        <style>
+            .timelink{
+                font-size: 30px;
+                text-decoration: none;
+            }    
+           
+
+        </style>
+        <div class="container-fluid bg-light text-light">
+            <div class="row p-2">
+                <div class="mx-auto"><img  src="imagens/logo.png" height="50px" width="50px"></div>
+                <div class="mr-0">
+                        <a class="btn btn-outline-dark py-0 my-3 botao" href="logout.php">Sair</a>
+                </div>
+            </div>
+        </div>
+        <div class="mx-auto mt-3 text-center">
+            <img src="imagens/logo-lol.png" height="20%" width="45%" class="mx-auto">
+        </div>
+        <div class="card border border-info mx-auto px-3 pt-3 pb-3" style="width: 60%;">
+                <h5 class="text-dark card-tittle">1. LCK</h5>
+                <p class="text-dark text-justify mx-0 card-text pb-3">A League of Legends dos Campeões da Coreia do Sul, popularmente conhecida como LCK , é a principal competição do esport no país. A liga surgiu em 2012, quando era chamada de League Of Legends Champions, sendo disputada em três competições por temporada, até passar por uma reestruturação no final de 2014. Atualmente, o campeonato é disputado por 10 equipes e dividido em duas temporadas por ano. O torneio dá qualificação para o Mid-Season Invitational (MSI) e para o Mundial.
+                        
+                        A LCK é considerada a liga mais forte do mundo, já que suas equipes dominam o cenário internacional desde 2013. A SKT Telecom T1, por exemplo, tem três títulos do Campeonato Mundial e dois no MSI, sendo o time mais vitorioso na história do League of Legends. A liga também conta com a maior quantidade de equipes que ganharam o Mundial.</p>
+                
+                <h5 class="text-dark card-tittle">2. LPL</h5>
+                <p class="text-dark text-justify mx-0 card-text pb-3">
+                        O League of Legends Pro League (LPL) da Tencent é o principal campeonato de LoL da China. Sua primeira temporada ocorreu em 2013 e, desde então, passou por diversas mudanças no formato, até atingir o modelo atual. A LPL possui 14 equipes divididas em dois grupos de sete times cada. Como uma das principais regiões do mundo no esport, a China possui três vagas diretas para o Campeonato Mundial de League of Legends, assim como a LCK.  </p>
+    
+                <h5 class="text-dark card-tittle">3. LCS</h5>
+                <p class="text-dark text-justify mx-0 card-text pb-3">
+                        League of Legends Championship Series (LCS) é o nome de duas ligas profissionais de League of Legends da Riot Games. Ao todo, são 20 equipes que disputam em duas competições separadas, a LCS NA (América do Norte) e a LCS EU (Europa).
+
+                        Criada em agosto de 2012, a liga possuía oito equipes em cada divisão, e foi apelidada de “Season 3”, por ter sido lançada no terceiro ano do LoL. O formato da competição foi ajustado ao longo dos anos. Atualmente, cada LCS é composta por 10 equipes que enfrentam todas as outras duas vezes ao longo da temporada, totalizando de 18 jogos cada. As partidas são disputadas no formato MD1, com cada time jogando uma vez por dia. A competição se estende por duas divisões: primavera e verão.
+                 </p>
+    
+                <h5 class="text-dark card-tittle">4. LMS</h5>
+                <p class="text-dark text-justify mx-0 card-text pb-3">
+                        A League of Legends Master Series (LMS) é a principal competição de LoL em Taiwan, Hong Kong e Macau. A liga foi fundada em 2015, quando se separou da GPL (Garena Premier League). A decisão foi tomada para aumentar as chances das equipes do sudeste asiático vencerem a competição, já que até o momento apenas times taiwaneses haviam sido campeões.
+                        O LMS possui 8 equipes, que se enfrentam em uma temporada dupla de round robin: cada time joga contra todos os outros duas vezes. As partidas em MD3 buscam a classificação para os playoffs. Desde sua primeira temporada, a LMS teve apenas duas equipes vitoriosas: Flash Wolves (com 5 títulos) e ahq e-Sports Club (com 2).
+                </p>
+    
+                <h5 class="text-dark card-tittle">5. VCS</h5>
+                <p class="text-dark text-justify mx-0 card-text">
+                        A Temporada de Primavera do Campeonato do Vietnã de League of Legends teve sua primeira edição em 2018. A região se separou da GLP recentemente e agora é independente no esport. A competição foi disputada por oito equipes, tendo uma fase de grupos de round robin, em partidas de MD3. Os quatro melhores times avançaram para a fase dos playoffs no formato de escalada, assim como acontece no CBLoL.
+                </p>
+                
+            </div>
+    
+            <div class="text-center mx-auto mt-4 mb-4">
+                <a href="index.html" class="badge badge-light mx-auto text-center">Home</a>
+            </div>
+
+        
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
